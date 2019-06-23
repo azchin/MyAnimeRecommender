8dcdd95b91adf2d1619f7d1256175a72c1a7785c
CorrectButNotRight <42951909+CorrectButNotRight@users.noreply.github.com>
2019-06-23 03:43:48 -0400

    update from Saturday

--- link.js
+++ link.js
@@ -1,7 +1,21 @@
 
+
+function formatData(input) {
+    var pairs = input.split("~");
+    var pLen = pairs.length - 1;
+    var str = "";
+    for(i = 0; i < pLen; ++i) {
+        var temp = pairs[i].split("`");
+        str = str + "<a href=\"" + temp[1] + "\">" + temp[0] + "</a><br>";
+    }
+    return str;
+}
+
 function getList(user) {
     var url;
-    url = "http://localhost:5000/?u=" + user;
+    url = "https://my-anime-recommender.herokuapp.com/?u=" + user;
+    //url = "http://localhost:5000/?u=" + user;
+    //url = "http://localhost:5000/test/";
     return url;
 }
 
@@ -10,11 +24,14 @@ $(document).ready(function () {
     $("#mainform").submit(function (event) {
         event.preventDefault();
         var user = $("#username").val();
-
+        /*$("#clicked").html(function() {
+            return "You clicked the button!";
+        });*/
         var url = getList(user);
         $.get(url, function (data) {
             $("#output").html(function () {
-                return data;
+                //return data;
+                return formatData(data);
             });
         });
     });
